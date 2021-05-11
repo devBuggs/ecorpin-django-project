f394a78e9e2718bdcae720d13ed2ce9984e5be3a
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-11 10:09:48 +0530

    latest issue (TypeError) solved
    TypeError at '/'
    
    Issue - 'name' keyword is missing in url pattern

--- ecorpin_corp/ecorpin/views.py
+++ ecorpin_corp/ecorpin/views.py
@@ -10,59 +10,78 @@ from .forms import feedback_form
 
 # Create your views here.
 def fevicon_ecorpin(request):
-    return 
+    pass
 
 def index_ecorpin(request):
     spotlightData = spotlight.objects.all().latest('spotlight_date')
-    context = {'latest_spotlight': spotlightData}
+    context = {
+        'latest_spotlight': spotlightData,
+    }
     return render(request, 'index.html', context)
 
 def privacy(request):
     info = endpoint_info.objects.get(end_point="Privacy Policy")
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'privacy.html', context)
 
 def team(request):
     info = endpoint_info.objects.get(end_point="Team")
     #Content Query
     ecorpians = list(ecorpian.objects.all().order_by('joinDate'))
-    context = {'info':info, 'ecorpians_list':ecorpians}
+    context = {
+        'info':info,
+        'ecorpians_list':ecorpians,
+    }
     return render(request, 'team.html', context)
 
 def tou(request):
     info = endpoint_info.objects.get(end_point="Terms of Use")
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'tou.html', context)
 
 def contact(request):
     info = endpoint_info.objects.get(end_point="Contact")
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'contact.html', context)
 
 def careers(request):
     info = endpoint_info.objects.get(end_point='Careers')
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'careers.html', context)
 
 def about(request):
     info = endpoint_info.objects.get(end_point='About')
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'about.html', context)
 
 def maintenance(request):
     info = endpoint_info.objects.get(end_point='Server Maintenance')
     #Content Query
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'maintenance.html', context)
 
 def covid(request):
     info = endpoint_info.objects.get(end_point='Ecorpin Covid-19 Response')
-    context = {'info':info}
+    context = {
+        'info':info,
+    }
     return render(request, 'covid.html', context)
 
 def feedback_create(request):
@@ -78,11 +97,11 @@ def feedback_create(request):
             print("-------------------------------------------------------")
     else:
         form = feedback_form()
-    context = { 'form': form, 'info': info}
+    context = {
+        'form': form,
+        'info': info,
+        }
     return render(request, 'feedback.html', context)
 
 def service(request):
     return HttpResponse("<h1>Ecorpin Secure Server - Services</h1>")
-
-def test(request):
-    return render(request, 'test.html', context=None)
