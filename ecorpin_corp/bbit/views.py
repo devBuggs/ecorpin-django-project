1ced55c5f64b5fbe2ee3c6ccfc5fdb54ac5f565a
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-11 16:43:17 +0530

    block-bit app section

--- ecorpin_corp/bbit/views.py
+++ ecorpin_corp/bbit/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index_bbit(request):
-    return HttpResponse("<h1>Ecorpin Secure Server - Block-Bit</h1>")
+    #return HttpResponse("<h1>Ecorpin Secure Server - Block-Bit</h1>")
+    return render(request, 'bbit/index.html', context=None)
