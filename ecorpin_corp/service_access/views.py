3023f46277b4fd9553616aef42ab2bc81d3a8f78
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-17 21:48:47 +0530

    templating in service access
    Login Service Access UI
    Contact Request UI

--- ecorpin_corp/service_access/views.py
+++ ecorpin_corp/service_access/views.py
@@ -11,7 +11,8 @@ from .forms import ContactRequestForm
 
 def service_access_view(request):
     #service access backend
-    return HttpResponse('<h1>Ecorpin Secure Server - Service Access</h1>')
+    #return HttpResponse('<h1>Ecorpin Secure Server - Service Access</h1>')
+    return render(request, 'service_access/serviceLogin.html', context=None)
 
 def contact_request(request):
     serverMsg = ''
@@ -19,12 +20,13 @@ def contact_request(request):
         form = ContactRequestForm(request.POST)
         if form.is_valid():
             form.save()
-            print("*******************************************************")
             serverMsg = "Contact Request is received. We'll contact you shortly. "
             return render(request, 'service_access/contact_request.html', { 'data':serverMsg })
         else:
-            print("-------------------------------------------------------")
+            print("--------> SOMETHING WENT WRONG.")
+            serverMsg = "Something went wrong! please try again... "
+            return render(request, 'service_access/contact_request.html', { 'data':serverMsg })
     else:
         form = ContactRequestForm()
-    context = { 'form': form, }
-    return render(request, 'service_access/contact_request.html', context)
+        context = { 'form': form, }
+        return render(request, 'service_access/contact_request.html', context)
