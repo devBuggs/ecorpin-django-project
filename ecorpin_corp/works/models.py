16cbb0c17246e7811f54ad71e930bdabe502e544
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-07-01 23:01:02 +0530

    Model url method..

--- ecorpin_corp/works/models.py
+++ ecorpin_corp/works/models.py
@@ -9,6 +9,9 @@ class ProjectsDetail(models.Model):
     description = models.CharField(max_length=200)
     url = models.CharField(max_length=100)
     ClientName = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        Return reverse(url_name, name=self.name)
 ''' 
 
 class ClientReview(models.Model):
