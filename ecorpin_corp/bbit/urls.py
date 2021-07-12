b522fe2a1cc1ddd3aff443198847505a892f2b43
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-07-12 21:05:36 +0530

    app_name variable added to all urls fils

--- ecorpin_corp/bbit/urls.py
+++ ecorpin_corp/bbit/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+#app_name = 'bbit'
 urlpatterns = [
     path('block_bit/', views.index_bbit, name='bbit'),
     
--- ecorpin_corp/service_access/urls.py
+++ ecorpin_corp/service_access/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 # import views here
 from . import views
 
+#app_name = 'service_access'
 urlpatterns = [
     path('service_access/', views.service_access_view, name='service_access'),
     path('contact_request', views.contact_request, name='contact_request'),
