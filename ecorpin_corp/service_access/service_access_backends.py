b67acdd31a6229d39407927f1f4ae265e8429f8a
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-07-23 22:02:26 +0530

    backend implementation

--- ecorpin_corp/service_access/service_access_backends.py
+++ ecorpin_corp/service_access/service_access_backends.py
@@ -7,12 +7,39 @@ from service_access.models import ServiceUser
 
 
 
+class ServiceAccessBackend(BaseBackend):
+    """
+    Authenticate against the settings ADMIN_LOGIN and ADMIN_PASSWORD.
 
-"""
-Authenticate against the settings ADMIN_LOGIN and ADMIN_PASSWORD.
+    Use the login name and a hash of the password. For example:
 
-Use the login name and a hash of the password. For example:
+    ADMIN_LOGIN = 'admin'
+    ADMIN_PASSWORD = 'pbkdf2_sha256$30000$Vo0VlMnkR4Bk$qEvtdyZRWTcOsCnI/oQ7fVOu1XAURIZYoOZ3iq8Dr4M='
+    """
+    def authenticate(self, request, service_id=None, password=None):
+        service_user_obj = ServiceUser.objects.filter(service_id = service_id)
+        username = None
+        if service_user_obj:
+            print("----------------->", service_user_obj)
+            username = str(service_user_obj.user.username)
+            # main backend
+            login_valid = (settings.ADMIN_LOGIN == username)
+            pwd_valid = check_password(password, settings.ADMIN_PASSWORD)
+            if login_valid and pwd_valid:
+                try:
+                    user = User.objects.get(username=username)
+                except User.DoesNotExist:
+                    # raise a error
+                    raise ValidationError("Invalid! pls try again..")
+                return user
+            return None
+        else:
+            raise ValueError("Service Id not found! check and try again..")
 
-ADMIN_LOGIN = 'admin'
-ADMIN_PASSWORD = 'pbkdf2_sha256$30000$Vo0VlMnkR4Bk$qEvtdyZRWTcOsCnI/oQ7fVOu1XAURIZYoOZ3iq8Dr4M='
-"""
+        
+    def get_user(self, user_id):
+        try:
+            #return User.objects.get(pk=user_id)
+            return ServiceUser.objects.get(pk = user_id)
+        except User.DoesNotExist:
+            return None
