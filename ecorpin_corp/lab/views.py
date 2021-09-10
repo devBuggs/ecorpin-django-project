a23f3863acf16cfd8cb3142df7ceb46d88aca073
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-09-10 10:55:57 +0530

    some fixs

--- ecorpin_corp/lab/views.py
+++ ecorpin_corp/lab/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
-        data = request.POST
+        data = dict(request.POST)
         print("----------> ", data)
+        user_mail = data['user_email'][0]
+        user_subject = data['user_subject'][0]
+        user_message = data['user_message'][0]
+        print("mail :: ", user_mail, "\nsubject :: ", user_subject, "\nmessage :: ", user_message)
+        email_from = settings.EMAIL_HOST_USER
+        recipitent_list = [user_mail, ]
+        send_mail(user_subject, user_message, email_from, recipitent_list)
+        return render(request, 'lab/index.html', {mail_response:'email send successfully...'})
+        #return HttpResponseRedirect('index')
     else:
-        context = {
-            'footer' : False,
-            "layout" : True
-        }
         return render(request, 'lab/index.html', context=None)
--- ecorpin_corp/service_access/views.py
+++ ecorpin_corp/service_access/views.py
@@ -7,6 +7,9 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 from service_access.models import ContactRequest, ServiceUser, Service
 from ecorpin.models import Endpoint_info
@@ -80,13 +83,33 @@ class ServiceDashboard(LoginRequiredMixin, View):
         return render(request, 'service_access/dashboard.html', context)
     def post(self, request):
         #<view logic>
+        user = request.user
         data = request.POST
-        print('----------------> ', data)
-        return redirect('ecorpin:feedback')
-
+        service_user_id = ServiceUser.objects.get(user = request.user.id)
+        service_list = Service.objects.filter(service_id = service_user_id)
+        payment_list = Transaction.objects.filter(user=request.user)
+        info = Endpoint_info.objects.get(end_point="Dashboard")
+        #print('----------------> ', data)
+        user_mail = request.user.email
+        user_subject = str(data['catogary'] +" | "+user.first_name+' '+user.last_name + " | "+ data['phone'] + " | Service Access")
+        #print('----------------> ', user_subject)
+        user_message = data['message']
+        #print('----------------> ', user_message)
+        print("mail :: ", user_mail, "\nsubject :: ", user_subject, "\nmessage :: ", user_message)
+        email_from = settings.EMAIL_HOST_USER
+        recipitent_list = [user_mail, ]
+        send_mail(user_subject, user_message, email_from, recipitent_list)
+        context = {
+            'payment_list': payment_list,
+            'service_list': service_list,
+            'info':info,
+            'mail_response': 'response submitted! thank you..'
+        }
+        return render(request, 'service_access/dashboard.html', context)
+        
 class ServiceUserProfile(LoginRequiredMixin, View):
     template_name = 'service_access/profile.html'
-    
+
     def get(self, request):
         #return reverse('ecorpin:team')
         #print("--------------------------- Service User Profile ---------------------------")
